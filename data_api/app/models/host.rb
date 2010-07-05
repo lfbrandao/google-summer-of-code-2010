5b9b2028f94f242bc8e3a6564227d9bf7f3312c4
luisbrandao <luisbrandao@gmail.com>
2010-07-05 17:57:26 +0100

    data api xml response working

--- data_api/app/models/host.rb
+++ data_api/app/models/host.rb
@@ -1,24 +1,29 @@
 class Host
-  attr_reader :id
-  attr_reader :name
-  attr_reader :label
-  attr_reader :value
-  
-  def self.get_all_websites
-    host_list = ActiveRecord::Base.connection.execute("select * " + APP_CONFIG['query_suffix'])
+   attr_reader :id
+   attr_reader :url
+   attr_reader :label
+   attr_reader :description
 
-    hosts = Array.new
-    host_list.each do |host_info|
-      hosts.push([host_info[0], host_info[1], host_info[2], host_info[3]])
-    end
-    
-    hosts.map { |host_info| new(host_info)}
-  end
-  
-  def initialize(host_info)
-    @id = host_info[0]
-    @name = host_info[1]
-    @label = host_info[2]
-    @value = host_info[3]
-  end
+   def self.all
+     host_list = ActiveRecord::Base.connection.execute("select * " + APP_CONFIG['query_suffix'])
+
+     hosts = Array.new
+     host_list.each do |host_info|
+       hosts.push([host_info[0], host_info[1], host_info[2], host_info[3]])
+     end
+
+     hosts.map { |host_info| new(host_info)}
+   end
+
+   def initialize(host_info)
+     @id = host_info[0]
+     @url = host_info[1]
+     @label = host_info[2]
+     @description = host_info[3]
+   end
+   
+   def to_xml(options = {})
+     xml = options[:builder] ||= Builder::XmlMarkup.new(:indent => options[:indent])
+     xml.host { |b| b.id(@id); b.url(@url); b.label(@label); b.description(@description) }
+   end
 end
--- data_api/config/routes.rb
+++ data_api/config/routes.rb
@@ -1,4 +1,6 @@
 ActionController::Routing::Routes.draw do |map|
+  map.resources :hosts
+
   # The priority is based upon order of creation: first created -> highest priority.
 
   # Sample of regular route:
@@ -40,5 +42,4 @@ ActionController::Routing::Routes.draw do |map|
   # consider removing or commenting them out if you're using named routes and resources.
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
-  map.resources :all_hosts
 end
