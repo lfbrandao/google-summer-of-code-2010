9977cf629f764a618a7828471a008a7b5b881e99
Luís Brandão <luisbrandao@gmail.com>
2010-07-16 19:43:59 +0100

    Data API working version 0.1 Todo: - authentication - sanitize sql - remove views and decide upon a default response format

--- data_api/app/controllers/hosts_controller.rb
+++ data_api/app/controllers/hosts_controller.rb
@@ -1,85 +1,25 @@
 class HostsController < ApplicationController
-  # GET /hosts
-  # GET /hosts.xml
+  # GET /hosts/index
+  # GET /hosts/index.xml
   def index
-    @hosts = Host.all
-
+    @hosts = Host.index
+    
     respond_to do |format|
       format.html # index.html.erb
-      format.xml  { render :xml => @hosts.to_xml}
+      format.xml  { render :xml => @hosts.to_xml(:root => "hosts")}
+      format.json  { render :json => @hosts.to_json}
     end
   end
 
-  # GET /hosts/1
-  # GET /hosts/1.xml
-  def show
-    @host = Host.find(params[:id])
+  # GET /hosts/find
+  # GET /hosts/find.xml
+  def filter
+    @hosts = Host.filter(params[:url])
 
     respond_to do |format|
       format.html # show.html.erb
       format.xml  { render :xml => @host}
-    end
-  end
-
-  # GET /hosts/new
-  # GET /hosts/new.xml
-  def new
-    @host = Host.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @host }
-    end
-  end
-
-  # GET /hosts/1/edit
-  def edit
-    @host = Host.find(params[:id])
-  end
-
-  # POST /hosts
-  # POST /hosts.xml
-  def create
-    @host = Host.new(params[:host])
-
-    respond_to do |format|
-      if @host.save
-        flash[:notice] = 'Host was successfully created.'
-        format.html { redirect_to(@host) }
-        format.xml  { render :xml => @host, :status => :created, :location => @host }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @host.errors, :status => :unprocessable_entity }
-      end
-    end
-  end
-
-  # PUT /hosts/1
-  # PUT /hosts/1.xml
-  def update
-    @host = Host.find(params[:id])
-
-    respond_to do |format|
-      if @host.update_attributes(params[:host])
-        flash[:notice] = 'Host was successfully updated.'
-        format.html { redirect_to(@host) }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @host.errors, :status => :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /hosts/1
-  # DELETE /hosts/1.xml
-  def destroy
-    @host = Host.find(params[:id])
-    @host.destroy
-
-    respond_to do |format|
-      format.html { redirect_to(hosts_url) }
-      format.xml  { head :ok }
+      format.json  { render :json => @hosts.to_json}
     end
   end
 end
--- data_api/app/models/host.rb
+++ data_api/app/models/host.rb
@@ -1,43 +1,65 @@
 class Host
-   attr_reader :id
-   attr_reader :url
-   attr_reader :label
-   attr_reader :description
+  attr_reader :info
+    
+  def self.index
+    puts 'index'
+    query = APP_CONFIG['base_query']
+    self.run_query_and_process_results(query)
+  end
 
-   def self.all
-     host_list = ActiveRecord::Base.connection.execute("select * " + APP_CONFIG['query_suffix'])
+  def self.filter(*args)
+    # todo - sanitize sql
+    if args[0].nil?
+      query = APP_CONFIG['base_query']
+    else
+      urls_from_args = args[0].split(',')  
+      base_query = APP_CONFIG['base_query'] + ' ' + APP_CONFIG['where_clause']
+      query = self.add_where_to_query(base_query, urls_from_args)
+    end
+    
+    self.run_query_and_process_results(query)
+  end
 
-     hosts = Array.new
-     host_list.each do |host_info|
-       hosts.push([host_info[0], host_info[1], host_info[2], host_info[3]])
-     end
-
-     hosts.map { |host_info| new(host_info)}
-   end
-
-   def self.xmltest
-      xml = File.read('/Users/luisbrandao/Documents/GSoC10/code/hostlist.xml')
-      # to-do - validation? schema,dtd
-     
-      #read xml
-      doc, hosts = REXML::Document.new(xml), []
-      doc.elements.each('hosts/host') do |element|
-        
-      uri = element.elements['uri'].text
-      type = element.elements['type'].text
-
+  def initialize(host_info)
+    @info = host_info
+  end
+   
+  def to_xml(options = {})
+    xml = options[:builder] ||= Builder::XmlMarkup.new(:indent => options[:indent])
+    field_names = APP_CONFIG['field_friendly_names']
+    xml.host do
+      field_names.each do |field|
+        eval("xml.#{field}(@info[field])")
       end
-   end
-   
-   def initialize(host_info)
-     @id = host_info[0]
-     @url = host_info[1]
-     @label = host_info[2]
-     @description = host_info[3]
-   end
-   
-   def to_xml(options = {})
-     xml = options[:builder] ||= Builder::XmlMarkup.new(:indent => options[:indent])
-     xml.host { |b| b.id(@id); b.url(@url); b.label(@label); b.description(@description) }
-   end
+    end
+    xml
+  end
+  
+  private
+  
+  def self.run_query_and_process_results(query)
+    host_list = ActiveRecord::Base.connection.execute(query)
+     
+    hosts = Array.new
+    field_names = APP_CONFIG['field_friendly_names']
+         
+    host_list.each do |host_info|
+      host_data = Hash.new
+      field_names.each_with_index do |field, index|
+        host_data[field] = host_info[index]
+      end
+      hosts.push(host_data)
+    end
+    
+    hosts.map { |host_info| new(host_info)}
+  end
+  
+  def self.add_where_to_query(query, url_array)
+    query << ' ('
+    url_array.each do |arg|
+      query << '\'' + arg + '\'' + ','
+      puts query
+    end
+    query = query.chop << ')'
+  end
 end
