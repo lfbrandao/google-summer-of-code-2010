7a4f10b32a1b523b32b2d13edecbde9f6fa2bc72
Luís Brandão <luisbrandao@gmail.com>
2010-08-05 19:50:48 +0100

    Switching the authentication to Authlogic

--- main_app/config/routes.rb
+++ main_app/config/routes.rb
@@ -1,4 +1,6 @@
 ActionController::Routing::Routes.draw do |map|
+  
+
   map.root :controller => 'hosts', :action => 'search'
   map.connect '', :controller => "hosts", :action => "search"
   map.connect 'hosts/search.:format', :controller => 'hosts', :action => 'search'
@@ -6,8 +8,10 @@ ActionController::Routing::Routes.draw do |map|
 
   map.resources :roles
   map.resources :repositories
-  map.devise_for :users
-
+  map.devise_for :users #,  :controllers => { :registrations => "registrations" }
+  map.connect 'hosts/index.:format', :controller => 'hosts', :action => 'index'
   #map.connect ':controller/:action/:id'
   #map.connect ':controller/:action/:id.:format'
+  map.connect 'users/', :controller => 'users', :action => 'index'
+  
 end
