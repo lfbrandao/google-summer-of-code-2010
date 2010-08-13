5754a6b8eb5dc902b987dc464d68a7f2e98650b8
Luís Brandão <luisbrandao@gmail.com>
2010-08-13 15:56:18 -0400

    Sql queries are now sanitized to avoid the injection of updates or deletes.

--- data_api/app/models/configuration.rb
+++ data_api/app/models/configuration.rb
@@ -1,2 +1,12 @@
 class Configuration < ActiveRecord::Base
+  validate :sanitize_sql
+  
+  def sanitize_sql
+    if query_base.downcase.include? 'update' or query_base.downcase.include? 'delete'  
+      errors.add(:query_base, "cannot include the words delete, update") 
+    end
+    if query_where.downcase.include? 'update' or query_where.downcase.include? 'delete'  
+      errors.add(:query_where, "cannot include the words delete, update") 
+    end
+  end
 end
